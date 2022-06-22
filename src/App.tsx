7dc5f61ad5ff60611db1ddf40decd11283d02c68
breznovic <slymarkel@gmail.com>
2022-06-22 15:32:57 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Route, Switch} from "react-router-dom"
+import {Home} from "./pages/Home/Home"
+import {MonthStatistics} from "./pages/MonthStatistics/MonthStatistics"
 
 function App() {
     return (
         <div>
-            Hello
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/month-statistics" exact component={MonthStatistics} />
+            </Switch>
         </div>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,12 @@
 import React, { useEffect } from 'react'
+import { useCustomDispatch, useCustomSelector } from '../../hooks/store'
+import { selectCurrentWeatherData } from '../../store/selectors'
+import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather'
+import { Days } from './components/Days/Days'
+import { ThisDay } from './components/ThisDay/ThisDay'
+import { ThisDayInfo } from './components/ThisDayInfo/ThisDayInfo'
+
+import s from './Home.module.scss'
 
 interface Props {}
 
