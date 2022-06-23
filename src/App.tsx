aef4d5525b8011d7f66005f72b2526b0f75ffee1
breznovic <slymarkel@gmail.com>
2022-06-23 19:53:47 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import {Route, Switch} from "react-router-dom"
 import {Home} from "./pages/Home/Home"
 import {MonthStatistics} from "./pages/MonthStatistics/MonthStatistics"
+import {Header} from "./shared/Header/Header";
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/month-statistics" exact component={MonthStatistics} />
